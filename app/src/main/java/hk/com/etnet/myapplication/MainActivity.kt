555c26ae685c77be2863cec3eb1e61b47f27b9be
Raymond Lin <raymondlin@live.hk>
2018-12-16 14:27:23 +0800

    first commit

---
+++ app/src/main/java/hk/com/etnet/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package hk.com.etnet.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import hk.com.etnet.myapplication.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/hk/com/etnet/myapplication/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package hk.com.etnet.myapplication.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
